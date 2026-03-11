a8153abfe4ef57ce866eda37d0daf10f12a4d1c9
anuhya <anuhyapadala2@gmail.com>
2026-03-11 11:53:52 +0530

    PatternsPrograms

--- Loops/while.py
+++ Loops/while.py
@@ -19,11 +19,27 @@
 #         print(i,'is even number')
 #     i+=1
 
-i=1
-while i<6:
-    print(' '*(5-i)+'* '*i)
-    i+=1
-i=4
-while i>0:
-    print(' '*(5-i)+'* '*i)
-    i-=1
+# i=1
+# while i<6:
+#     print(' '*(5-i)+'* '*i)
+#     i+=1
+# i=4
+# while i>0:
+#     print(' '*(5-i)+'* '*i)
+#     i-=1
+
+
+# for i in range(1,6):
+#     for j in range(1,i+1):
+#         print(j,end=' ')   
+#     print() 
+
+
+# i=1
+# while i<=5:
+#     j=1
+#     while j<=i:
+#         print(j,end=' ')
+#         j+=1
+#     print()
+#     i+=1
